573bf965a12baf1ff423a396dc3218273a9a322b
DreadDev1 <kameen.avery@gmail.com>
2025-08-05 00:01:04 -0400

    Create BaseAbilitySystemComponent and BaseAttributeSet

--- Source/BasePrototype/BasePrototype.Build.cs
+++ Source/BasePrototype/BasePrototype.Build.cs
@@ -8,7 +8,7 @@ public class BasePrototype : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" ,"UMG",  "Inventory" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" ,"UMG",  "Inventory", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
